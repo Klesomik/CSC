0733de5d10bca1109d346b8ceea98161047a2b58
Klesomik <neon_76@mail.ru>
2017-06-08 12:10:11 +0300

    Added example for project
    Statistics is ready

---
+++ Src/Result/test.cpp
@@ -0,0 +1,12 @@
+int main ()
+{
+    int a = 5;
+
+    if (a == 5)
+        return 12;
+
+                            if (a == 10)
+                                return 5;
+
+    return 0;
+}
---
+++ Src/Result/test.cpp.res
@@ -0,0 +1,9 @@
+int main ()
+{
+    int a = 5;
+                if  (a == 5)
+        return 12;
+                if  (a == 10)
+                                return 5;
+    return 0;
+}
